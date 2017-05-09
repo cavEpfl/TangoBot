fef1c8205c54b12e35627b0e3d6987a4dee38881
Alexandre Cavaleri <alexandre.cavaleri@epfl.ch>
2017-05-09 20:29:03 +0200

    extract entries works separating entries works still needs to remove identical duplicates correctly and sort back the dates correctly

--- TangoBot.py
+++ TangoBot.py
@@ -2,15 +2,21 @@ import urllib
 import requests
 import configparser
 from bs4 import BeautifulSoup
+from datetime import datetime
+from difflib import Differ
+
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+#Login data
 user=config['DEFAULT']['userName']
 passw=config['DEFAULT']["password"]
 baseurl='http://wikipast.epfl.ch/wikipast/'
 summary='Wikipastbot update'
-names=['Madame X','Monsieur Y']
+
+#Replace this using listepagesbot.py to get all page names to go through
+names=['Naissance']
 
 # Login request
 payload={'action':'query','format':'json','utf8':'','meta':'tokens','type':'login'}
@@ -28,3 +34,57 @@ edit_token=r3.json()['query']['tokens']['csrftoken']
 
 edit_cookie=r2.cookies.copy()
 edit_cookie.update(r3.cookies)
+
+def main():
+
+    # 1. retrouver le contenu et l'imprimer
+
+    for name in names:
+        result = requests.post(baseurl + 'api.php?action=query&titles=' + name + '&export&exportnowrap')
+        soup = BeautifulSoup(result.text, "lxml")
+        # soup=BeautifulSoup(result.text)
+        dataStr = ''
+        for primitive in soup.findAll("text"):
+            if primitive.string is not None:
+                dataStr += primitive.string
+
+        entries = getEntries(dataStr)
+        removeDuplicates(entries)
+
+def getEntries(data):
+    lines = data.split('\n')
+    entries = []
+    for line in lines:
+        if isValidEntry(line):
+            entries.append(line)
+    return entries
+
+
+def isValidEntry(entry):
+    if entry[0:3] == '*[[' and (entry[3:7] + entry[8:10] + entry[11:13]).isdigit() and entry[13:15] == ']]' and entry[7] == '.' and entry[10] == '.':
+        return True
+    else:
+        return False
+
+
+def removeDuplicates(entries):
+
+    finalEntries = list(set(entries)).sort(key=lambda x: datetime.strptime(cleanDate(x), '%Y%m%d'))
+
+    print(finalEntries)
+
+    code2 = ''
+    for entry in finalEntries:
+        code2 += entry
+
+    print("\nAfter:\n" + code2)
+
+def cleanDate(str):
+
+    finalStr = ''
+    dateStr = str.partition("/")[0]
+    finalStr.join(e for e in dateStr if e.isalnum())
+    print(finalStr)
+    return "20000203"
+
+main()
